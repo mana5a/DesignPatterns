60e995146a782bf188f5682f4195849f40f16e1d
mana5a <manasajagadeesh98@gmail.com>
2019-12-14 17:39:06 +0530

    mutation2

--- genetic.cpp
+++ genetic.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-#define POPULATION_SIZE 50
+#define POPULATION_SIZE 10
 
 #define NUM_CITY 4
 
@@ -18,84 +18,119 @@ int random_num(int start, int end)
     return random_int; 
 } 
 
+bool compare(const pair<vector <int>, int>&i, const pair<vector <int>, int>&j)
+{
+    return i.second < j.second;
+}
+
 class Genome
 {
     private:
     vector<vector<int>> dist_mat;
-    vector<chromosome> population;
+    vector<pair<chromosome, float>> population;
     
     public:
+
+    float fitness(chromosome chr)
+    {
+        float distance=0;
+        for(int i=0;i<chr.size()-1;++i)
+        {
+            distance+=dist_mat[chr[i]][chr[i+1]];
+        }
+        return distance;
+    }
+
     void init_population()
     {
-        for(int i=0;i<POPULATION_SIZE;i++)
+        for(int i=0;i<POPULATION_SIZE;++i)
         {
             vector <int> v=prototype;
-            vector <int> v1;
+            chromosome v1;
             v1.push_back(0);
             //cout<<"Size"<<v.size();
-            for(int j=0;j<NUM_CITY-1;j++)
+            for(int j=0;j<NUM_CITY-1;++j)
             {
                 int r=random_num(0,v.size());
                 v1.push_back(v[r]);
                 v.erase(v.begin()+r);
             }
             v1.push_back(0);
-            population.push_back(v1);
+            float fit=fitness(v1);
+            population.push_back(make_pair(v1,fit));
         }
     }
     Genome()
     {
-        init_population();
         dist_mat={{ 0, 10, 15, 20 }, 
                 { 10, 0, 35, 25 }, 
                 { 15, 35, 0, 30 }, 
                 { 20, 25, 30, 0 } };
+        init_population();
     }
     void display_population()
     {
-        cout<<"Population\n";
-        cout<<"Size:"<<population.size()<<"\n";
-        for (int i = 0; i < population.size(); i++)
+        //cout<<"Population\n";
+        //cout<<"Size:"<<population.size()<<"\n";
+        for (int i = 0; i <POPULATION_SIZE; ++i)
         {
-            for (int j = 0; j < population[i].size(); j++)
+            for (int j = 0; j < population[i].first.size(); ++j)
             {
-                cout << population[i][j]<<" ";
+                cout << population[i].first[j]<<" ";
             }
+            cout<<population[i].second<<"\n";
             // cout<<"Distance: "<<fitness(population[i]);
-            cout<<"Mutation:";
-            display_chromosome(mutation(population[i]));
+            // cout<<"Mutation:";
+            // display_chromosome(mutation(population[i].first),population[i].second);
             cout<<"\n";
         }
     }
 
-    void display_chromosome(chromosome chr)
+    void display_chromosome(chromosome chr,float fit)
     {
-        for (int j = 0; j < chr.size(); j++)
+        for (int j = 0; j < chr.size(); ++j)
         {
             cout << chr[j]<<" ";
         }
+        cout<<"\tFitness:"<<fit<<"\n";
     }
+
     void display_dist_mat()
     {
         cout<<"Distance Matrix\n";
-        for (int i = 0; i < dist_mat.size(); i++)
+        for (int i = 0; i < dist_mat.size(); ++i)
         {
-            for (int j = 0; j < dist_mat[i].size(); j++)
+            for (int j = 0; j < dist_mat[i].size(); ++j)
             {
                 cout << dist_mat[i][j]<<" ";
             }
             cout<<"\n";
         }
     }
-    float fitness(chromosome chr)
+
+    void run_GA()
     {
-        int distance=0;
-        for(int i=0;i<chr.size()-1;i++)
+        cout<<"Initial Population\n";
+        display_population();
+        for(int i=0;i<POPULATION_SIZE;++i)
         {
-            distance+=dist_mat[chr[i]][chr[i+1]];
+            
+            //display_chromosome(population[i].first,population[i].second);
+            //cout<<"\n";
+            mutation(population[i].first);
+            population[i].second=fitness(population[i].first);
+            
+            //display_chromosome(population[i].first,population[i].second);
+            //cout<<"\n";
+            
         }
-        return 1/distance;
+        cout<<"After Mutation\n";
+        display_population();
+        selection();
+        cout<<"After Selection\n";
+        display_population();
     }
+    
 
     chromosome mutation(chromosome chr)
     {
@@ -107,7 +142,49 @@ class Genome
         return chr;
     }
 
-    
+    void selection()
+    {
+        int sum=0;
+        vector <pair<vector<int>,float>> ump;
+        for(int i=0;i<POPULATION_SIZE;++i)
+        {
+            int val=fitness(population[i].first);
+            //cout<<"val"<<val;
+            sum+=val;
+        }
+        cout<<"sum:"<<sum<<"\n";
+        vector <pair<vector<int>,float>> topk;
+        for(int i=0;i<POPULATION_SIZE;++i)
+        {
+            int val=fitness(population[i].first);
+            float select_prob=val/sum;
+
+            topk.push_back(make_pair(population[i].first,select_prob));
+            sort(topk.begin(),topk.end(),compare);
+
+            
+
+        }
+        for(int i=0;i<POPULATION_SIZE;++i)
+        {
+            ump.push_back(make_pair(topk[i].first,fitness(topk[i].first)));
+        }
+        // vector <vector<int>> new_pop;
+        // for(int i=0;i<population.size();++i)
+        // {
+        //     new_pop.push_back(ump[i].first);
+        //}
+        population=ump;
+        // cout<<"Selection:\n";
+        // for(int i=0;i<population.size();++i)
+        // {
+        //     display_chromosome(population[i].first, population[i].second);
+        //     cout<<"\n";
+        // }
+        
+    }
+
+
 
 };
 
@@ -115,6 +192,8 @@ class Genome
 int main()
 {
     Genome gene;
-    gene.display_population();
-    gene.display_dist_mat();
+    gene.run_GA();
+    //gene.display_population();
+    //gene.display_dist_mat();
+    //gene.selection();
 }
